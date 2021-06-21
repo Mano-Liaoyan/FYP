238f24a1ef2b9e241b4edcdf7666e840513f1eb6
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-06-21 11:29:22 +0800

    Finish task page prototype

---
+++ Assets/Scripts/PagePanelManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is used to manage the show and hide of different UI interfaces
+/// </summary>
+public class PagePanelManager : MonoBehaviour
+{
+    public void SwitchPanel()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
