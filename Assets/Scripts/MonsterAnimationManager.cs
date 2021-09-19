f035a8cfa05973fffd6d30776db7460f4ec11aa2
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-09-19 23:55:34 +0800

    finish crab

--- Assets/Scripts/MonsterAnimationManager.cs
+++ Assets/Scripts/MonsterAnimationManager.cs
@@ -9,14 +9,21 @@ using UnityEngine.Serialization;
 [RequireComponent(typeof(Animator))]
 public class MonsterAnimationManager : MonoBehaviour
 {
-    private Animator _animator;
     public float moveSpeed = 100.0f;
+    public GameObject face;
+    public GameObject mouth;
+
+    public Sprite[] faces;
+    public Sprite[] mouths;
+
+    private Animator _animator;
+    private SpriteRenderer _faceRenderer;
+    private SpriteRenderer _mouthRenderer;
 
     private float _xAxis;
     private float _yAxis;
-    private static readonly int Move = Animator.StringToHash("Move");
-    // ReSharper disable once InconsistentNaming
-    private static readonly int LMClick = Animator.StringToHash("Left Mouse Click");
+    private float _xPrev;
+    private int _movingFlag = 1;
 
     private const string IDLE = "Idle";
     private const string HURT = "Hurt";
@@ -24,11 +31,13 @@ public class MonsterAnimationManager : MonoBehaviour
     private const string WALK = "Walking";
     private const string IDLE_BLINK = "Idle Blinking";
     private const string SMOKE = "Smoke";
-    
+
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _faceRenderer = face.GetComponent<SpriteRenderer>();
+        _mouthRenderer = mouth.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -37,35 +46,54 @@ public class MonsterAnimationManager : MonoBehaviour
         // Test usage: to check the animation
         TestAnimation();
     }
+
     private void TestAnimation()
     {
-        
+        if (_animator.GetBool("IsIdle"))
+        {
+            _faceRenderer.sprite = faces[0];
+            _mouthRenderer.sprite = mouths[0];
+        }
+
         _xAxis = Input.GetAxis("Horizontal");
         _yAxis = Input.GetAxis("Vertical");
+
         // Test Walking
-        if (_xAxis != 0 || _yAxis!=0)
+        if (_xAxis != 0 || _yAxis != 0)
         {
-            // _animator.Play(WALK);
-            _animator.SetTrigger(Move);
-            transform.Translate(new Vector3(_xAxis,_yAxis,0) * Time.deltaTime * moveSpeed);
+            if (_xPrev * _xAxis < 0)
+            {
+                gameObject.transform.Rotate(Vector3.up, 180);
+                _movingFlag = -_movingFlag;
+            }
+
+            _animator.SetTrigger("Move");
+            transform.Translate(new Vector3(_xAxis * _movingFlag, _yAxis, 0) * Time.deltaTime * moveSpeed);
+
+            _xPrev = _xAxis == 0 ? _xPrev : _xAxis;
         }
+
         // Test Attacking
         if (Input.GetMouseButtonUp(0))
         {
             // _animator.Play(ATTACK);
-            _animator.SetTrigger(LMClick);
+            _animator.SetTrigger("LeftMouseClick");
         }
-        
+
         // Test Hurt
         if (Input.GetKeyUp(KeyCode.V))
         {
             _animator.Play(HURT);
+            _faceRenderer.sprite = faces[1];
+            _mouthRenderer.sprite = mouths[1];
         }
-        
+
         // Test Smoke
         if (Input.GetKeyUp(KeyCode.Space))
         {
             _animator.Play(SMOKE);
+            _faceRenderer.sprite = faces[2];
+            _mouthRenderer.sprite = mouths[1];
         }
     }
 }
