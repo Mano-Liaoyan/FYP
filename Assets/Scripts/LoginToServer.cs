82c58b3a47c84b8f7d2ca6b2f17e62fb2245ee1c
Mano <ben.jaminlyyf21@gmail.com>
2021-10-08 21:22:59 +0800

    Finish Login Error Message

--- Assets/Scripts/LoginToServer.cs
+++ Assets/Scripts/LoginToServer.cs
@@ -11,11 +11,15 @@ public class LoginToServer : MonoBehaviour
     private const string Host = "20.94.197.30";
     private const int Port = 7350;
     private const string ServerKey = "defaultkey";
-    
+
     private const string PrefKeyName = "nakama.session";
 
     private readonly IClient _client = new Client(Scheme, Host, Port, ServerKey);
+
     private ISession _session;
+    [SerializeField] private GameObject WarningMessage;
+    [SerializeField] private GameObject PopupWindow;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,28 +29,44 @@ public class LoginToServer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public async void Login(string username, string pwd) {
-        // Debug.Log($"username: {username.text},password: {password.text}");
-        // string email = "1234@mail.com";
-        // string pwd = "password";
-        try {
+    /// <summary>
+    /// Login user.
+    /// </summary>
+    /// <returns>
+    /// None
+    /// </returns>
+    /// <param name="username">
+    /// The username or email
+    /// </param>
+    /// <param name="pwd">
+    /// The user's password
+    /// </param>
+    public async void Login(string username, string pwd)
+    {
+        try
+        {
             var session = await _client.AuthenticateEmailAsync(username, pwd);
             Debug.Log($"New user: {session.Created}, {session}");
-        } catch(ApiResponseException e){
+            // If success, switch scene to game view
+        }
+        catch (ApiResponseException e)
+        { // Pop up a modal that infos user the current error
             print(e);
+            PopupWindow.SetActive(true);
+            WarningMessage.GetComponent<TMP_Text>().text = e.StatusCode + "\n" + e.Message;
         }
 
-        
+
     }
-    public void OnClick(){
+    public void OnClick()
+    {
         GameObject inputUsernameObject = GameObject.Find("InputField_ID/TextArea/Text");
         GameObject inputPasswordObject = GameObject.Find("InputField_Pssword/TextArea/Text");
         string inputUsername = inputUsernameObject.GetComponent<TMP_Text>().text;
         string inputPassword = inputPasswordObject.GetComponent<TMP_Text>().text;
 
         Login(inputUsername, inputPassword);
-        print(inputUsername);
     }
 }
