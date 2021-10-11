2bf0e34df9ba8396fac893a4a3f4f907daefff87
Mano <ben.jaminlyyf21@gmail.com>
2021-10-11 23:33:48 +0800

    fix UI prefabs location problem

--- Assets/Scenes/Battle/Scripts/FriendlyChracter.cs
+++ Assets/Scenes/Battle/Scripts/FriendlyChracter.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class FriendlyChracter : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> characters;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject Character_Friendly = GameObject.Find("Character_Friendly");
+        foreach (GameObject character in characters)
+        {
+            // Set worldPostionStays to false
+            // See https://blog.csdn.net/qq_42672770/article/details/109180796
+            GameObject NewCharacter = Instantiate(character, transform.position, Quaternion.identity);
+            NewCharacter.transform.SetParent(Character_Friendly.transform, false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void CalcPostion()
+    {
+
     }
 }
