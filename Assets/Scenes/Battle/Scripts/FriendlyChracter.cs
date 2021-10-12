576ec4f766589e5348c0bd3e603a5bc0012a3084
Mano <ben.jaminlyyf21@gmail.com>
2021-10-12 16:19:58 +0800

    modify placing scripts

--- Assets/Scenes/Battle/Scripts/FriendlyChracter.cs
+++ Assets/Scenes/Battle/Scripts/FriendlyChracter.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class FriendlyChracter : MonoBehaviour
 {
     [SerializeField] private List<GameObject> characters;
+    private GameObject Character_Friendly;
+    private RectTransform parentRectTransform;
+    private float screenWidth;
+    private float screenHeight;
+    private Vector2 parentSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject Character_Friendly = GameObject.Find("Character_Friendly");
+        Character_Friendly = GameObject.Find("Character_Friendly");
+        parentRectTransform = Character_Friendly.GetComponent<RectTransform>();
+        parentSize = parentRectTransform.rect.size;
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
         foreach (GameObject character in characters)
         {
             // Set worldPostionStays to false
             // See https://blog.csdn.net/qq_42672770/article/details/109180796
             GameObject NewCharacter = Instantiate(character, transform.position, Quaternion.identity);
             NewCharacter.transform.SetParent(Character_Friendly.transform, false);
+            int index = characters.IndexOf(character);
+            CalcPostion(NewCharacter, index);
+
         }
     }
 
@@ -25,8 +37,16 @@ public class FriendlyChracter : MonoBehaviour
 
     }
 
-    void CalcPostion()
+    void CalcPostion(GameObject obj, int index)
     {
-
+        //print($"index: {index} size: {parentSize}");
+        //float x = screenWidth / 4.0f;
+        float x = parentSize.x / 4.0f;
+        //float y = ((float)(index + 1) / (float)characters.Count) * screenHeight;
+        float y = ((float)(index + 1) / (float)characters.Count) * parentSize.y;
+        print($"x = {x},y = {y}");
+        //RectTransform objRect = obj.GetComponent<Transform>() as RectTransform;
+        //objRect.sizeDelta = new Vector3(x, y);
+        obj.transform.localPosition = new Vector3(0, y - parentSize.y, 0);
     }
 }
