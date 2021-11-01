dc07506eadb3753b7dcd0c9e77d19028206c32cd
Mano <ben.jaminlyyf21@gmail.com>
2021-11-01 19:53:48 +0800

    Add Mage attack

--- Assets/Scenes/Battle/Scripts/NecromancerMagic.cs
+++ Assets/Scenes/Battle/Scripts/NecromancerMagic.cs
@@ -8,7 +8,7 @@ public class NecromancerMagic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        EventCenter.Instance.AddEventListener("NecromancerAttack", Attack);
+
     }
 
     // Update is called once per frame
--- Assets/Scenes/Battle/Scripts/Test/MageCharacter.cs
+++ Assets/Scenes/Battle/Scripts/Test/MageCharacter.cs
@@ -7,12 +7,23 @@ public class MageCharacter : Character
     void Start()
     {
         myPosition = transform.localPosition;
-        //EventCenter.Instance.AddEventListener<string, int>("RotateCharacter", RotateCharacter);
     }
 
     public override void Attack(Vector3 endPoint)
     {
-        StartCoroutine(IEMoveCharacter(myPosition, endPoint));
+        StartCoroutine(MageAttack(endPoint));
+    }
+
+    public IEnumerator MageAttack(Vector3 endPoint)
+    {
+        Vector3 z_offset = new Vector3(0, 0, -20);
+        Quaternion y_rotate = Quaternion.Euler(0, 90, 0);
+        GameObject skill = (GameObject)Instantiate(Resources.Load($"Prefab/VFX/Modify Magic Ice"),
+            transform.position + z_offset, y_rotate);
+        yield return new WaitForSeconds(2f);
+        EventCenter.Instance.TriggerEventListener("ActiveSlots");
+        Destroy(skill);
+        
     }
 
 }
