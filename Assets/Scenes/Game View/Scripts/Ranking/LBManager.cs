a5038a308f16dab6ae9e1578778f5cba26afcb40
Mano <ben.jaminlyyf21@gmail.com>
2021-11-09 22:12:07 +0800

    change ranking script directory

--- Assets/Scenes/Game View/Scripts/Ranking/LBManager.cs
+++ Assets/Scenes/Game View/Scripts/Ranking/LBManager.cs
@@ -9,25 +9,27 @@ public class LBManager : MonoBehaviour
 {
     GameObject ScrollRect;
     GameObject Content;
-    List<GameObject> List_me = new List<GameObject>();
+    List<GameObject> List_me;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        List_me = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    
-    public void getLeaderboard() {
-        _ = getLeaderboardAsync();
+
     }
 
-    public async Task getLeaderboardAsync() {
+    public async void getLeaderboard()
+    {
+        await getLeaderboardAsync();
+    }
+
+    public async Task getLeaderboardAsync()
+    {
         // 需要更改default选项
         // 还要加username
         const string leaderboardId = "score";
@@ -35,13 +37,15 @@ public class LBManager : MonoBehaviour
         ScrollRect = GameObject.Find("ScrollRect");
         Content = GameObject.Find("Content");
         print(Content);
-        foreach (Transform child in Content.transform) {
+        foreach (Transform child in Content.transform)
+        {
             List_me.Add(child.gameObject);
         }
 
         var my_rank = await User.client.ListLeaderboardRecordsAroundOwnerAsync(User.session, leaderboardId, User.session.UserId, null);
-        
-        foreach (var r in my_rank.Records) {
+
+        foreach (var r in my_rank.Records)
+        {
             ScrollRect.transform.Find("List_Me/Text_999").GetComponent<TMP_Text>().text = r.Rank;
             ScrollRect.transform.Find("List_Me/Text_Score").GetComponent<TMP_Text>().text = r.Score;
             Debug.LogFormat("My score: Record for '{0}' score '{1}'", r.Username, r.Score);
@@ -49,9 +53,10 @@ public class LBManager : MonoBehaviour
         int counter = 0;
 
         var result = await User.client.ListLeaderboardRecordsAsync(User.session, leaderboardId);
-        
-        
-        foreach (var r in result.Records) {
+
+
+        foreach (var r in result.Records)
+        {
 
             print(List_me[counter]);
             List_me[counter].transform.Find("Text_Score").GetComponent<TMP_Text>().text = r.Score;
@@ -59,17 +64,20 @@ public class LBManager : MonoBehaviour
             Debug.LogFormat("Record for '{0}' score '{1}'", r.Username, r.Score);
             counter++;
         }
-        if (result.NextCursor != null) {
-            result = await User.client.ListLeaderboardRecordsAsync(User.session, leaderboardId, null, null,10,result.NextCursor);
+        if (result.NextCursor != null)
+        {
+            result = await User.client.ListLeaderboardRecordsAsync(User.session, leaderboardId, null, null, 10, result.NextCursor);
             //User.client.ListLeaderboardRecordsAsync()
-            foreach (var r in result.Records) {
+            foreach (var r in result.Records)
+            {
                 Debug.LogFormat("Record for '{0}' score '{1}'", r.Username, r.Score);
                 List_me[counter].transform.Find("Text_Score").GetComponent<TMP_Text>().text = r.Score;
                 print(List_me[counter].transform.Find("Text_Score"));
                 counter++;
             }
         }
-        for(int i=counter; i<9; i++) {
+        for (int i = counter; i < 9; i++)
+        {
 
         }
     }
